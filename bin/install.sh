c1ad9f1d6e88b68992a068b0d69cc5d2ba19aa29
Jinhwa Joung <jjangun@gmail.com>
2019-02-11 11:22:35 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -2,10 +2,10 @@
 MType=`uname`
 
 echo "Append config .vimrc"
-echo "source $HOME/tools/env/.vimrc" >> $HOME/.vimrc
+echo "source $HOME/.tools/env/.vimrc" >> $HOME/.vimrc
 
 echo "Append config tools.sh"
-echo "source $HOME/tools/bin/tools.sh" >> $HOME/.bashrc
+echo "source $HOME/.tools/bin/tools.sh" >> $HOME/.bashrc
 
 echo "Downloading... Vundle"
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
@@ -32,24 +32,24 @@ if [ "$MType" == *"CYGWIN"* ]
 then
     echo "Cygwin System"
     echo "Downloading... Global Win32 Version"
-    mkdir -p $HOME/tools/bin/global
-    git clone https://github.com/jjangun/GLOBAL_Win32.git $HOME/tools/bin/global
+    mkdir -p $HOME/.tools/bin/global
+    git clone https://github.com/jjangun/GLOBAL_Win32.git $HOME/.tools/bin/global
 else
     echo "Install gnu global"
     global_version="global-6.6.3"
     global_archive=$global_version".tar.gz"
     global_down_url="http://ftp.gnu.org/gnu/global/"$global_archive
 
-    mkdir -p $HOME/tools/bin/global
+    mkdir -p $HOME/.tools/bin/global
     wget $global_down_url -P $HOME
     tar xvzf $global_archive -C $HOME
 
     cd $HOME/$global_version
-    ./configure --prefix=$HOME/tools/bin/global
+    ./configure --prefix=$HOME/.tools/bin/global
     make
     make install
 
-    cp $HOME/tools/bin/global/share/gtags/gtags.conf $HOME/.globalrc
+    cp $HOME/.tools/bin/global/share/gtags/gtags.conf $HOME/.globalrc
 
     echo "Remove install files"
     rm -rf $HOME/$global_archive
