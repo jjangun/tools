d05338db7b00479c57ba1bace7eb584f365f7854
Jinhwa Joung <jinhwa.joung@mobis.co.kr>
2019-02-18 13:37:11 +0900

    Update tool.sh

--- bin/tools.sh
+++ bin/tools.sh
@@ -9,8 +9,6 @@ source acd_func.sh
 
 # for zsh {{{
 if [[ "$SHELL" =~ "zsh" ]]; then
-    echo "for zsh shell"
-
     prompt_context() {
         if [[ "$USER" != "$DEFAULT_USER" || -n "$SSH_CLIENT" ]]; then
             prompt_segment black default "%(!.%{%F{yellow}%}.)$USER"
